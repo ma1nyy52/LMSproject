1ee2901a57ffa51a709f785ec1b5d9171e647d92
itbortnik <itbortnik@gmail.com>
2025-01-26 17:20:40 +0300

    изменены стрелки в меню уровней

--- a.py
+++ a.py
@@ -2,15 +2,21 @@ import pygame
 import os
 import sys
 
+# Функция для загрузки изображений
 def load_image(name, colorkey=None):
-    if not os.path.isfile(name):
-        print(f"Файл с изображением '{name}' не найден")
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
-    image = pygame.image.load(name)
+    image = pygame.image.load(fullname)
     return image
 
 pygame.init()
-pygame.mixer.init()
+
+# Загрузка музыки
+def load_music():
+    pygame.mixer.music.load(os.path.join('data', 'game_music.mp3'))  # Замените на имя вашего файла
+    pygame.mixer.music.play(-1)  # Воспроизводить в бесконечном цикле
 
 # Константы
 TILE_SIZE = 50
@@ -27,10 +33,9 @@ BROWN = (139, 69, 19)
 GREEN = (0, 128, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
-BLUE = (0, 0, 255)
-PURPLE = (128, 0, 128)
+BLUE = (0, 0, 255)  # Цвет для кнопки
+PURPLE = (128, 0, 128)  # Цвет для двери
 
-# Загрузка изображений
 player_img = load_image("player.png")
 box_img = load_image("box.png")
 wall_img = load_image("brick.png")
@@ -39,8 +44,9 @@ floor_img = pygame.Surface((TILE_SIZE, TILE_SIZE))
 floor_img.fill(WHITE)
 button_img = load_image("button.png")
 door_img = load_image("door.png")
+teleport_img = load_image("teleport.png")
 
-# Уровни (0 - пол, 1 - стена, 2 - ящик, 3 - цель, 4 - игрок, 5 - кнопка, 6 - дверь)
+# Уровни (0 - пол, 1 - стена, 2 - ящик, 3 - цель, 4 - игрок, 5 - кнопка, 6 - дверь, 7 - телепорт)
 levels = [
     [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -54,16 +60,46 @@ levels = [
     ],
     [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 7, 0, 0, 0, 1],  # Телепорт
+        [1, 0, 2, 0, 1, 0, 2, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 3, 1],
+        [1, 0, 2, 0, 0, 4, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 7, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+    [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
+        [1, 0, 0, 0, 0, 4, 0, 2, 0, 1],
+        [1, 0, 6, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+[
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 2, 0, 1, 0, 2, 0, 0, 1],
         [1, 0, 0, 0, 1, 0, 0, 0, 3, 1],
-        [1, 0, 2, 2, 0, 4, 0, 0, 0, 1],
+        [1, 0, 2, 0, 0, 4, 0, 0, 0, 1],
         [1, 0, 0, 0, 1, 0, 0, 2, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ],
     [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 7, 0, 0, 0, 1],  # Телепорт
+        [1, 0, 2, 0, 1, 0, 2, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 3, 1],
+        [1, 0, 2, 0, 0, 4, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 7, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+    [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
         [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
@@ -72,62 +108,26 @@ levels = [
         [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ],
-
-    [
+[
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
-        [1, 0, 0, 0, 0, 4, 0, 2, 0, 1],
-        [1, 0, 6, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 2, 0, 1, 0, 2, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 3, 1],
+        [1, 0, 2, 0, 0, 4, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-,
+    ],
     [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 7, 0, 0, 0, 1],  # Телепорт
+        [1, 0, 2, 0, 1, 0, 2, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 3, 1],
+        [1, 0, 2, 0, 0, 4, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 7, 2, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
-        [1, 0, 0, 0, 0, 4, 0, 2, 0, 1],
-        [1, 0, 6, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-,
-    [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
-        [1, 0, 0, 0, 0, 4, 0, 2, 0, 1],
-        [1, 0, 6, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-,
-    [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
-        [1, 0, 0, 0, 0, 4, 0, 2, 0, 1],
-        [1, 0, 6, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-,
-    [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 2, 5, 1, 0, 2, 0, 3, 1],
-        [1, 0, 0, 0, 0, 4, 0, 2, 0, 1],
-        [1, 0, 6, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-,
+    ],
     [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -148,6 +148,8 @@ def load_level():
     player_pos = None
     boxes = []
     goals = []
+    teleport_pairs = {}
+    lst = []
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
             if current_level[y][x] == 4:
@@ -156,6 +158,11 @@ def load_level():
                 boxes.append([x, y])
             elif current_level[y][x] == 3:
                 goals.append([x, y])
+            elif current_level[y][x] == 7:
+                lst.append((x, y))
+    if lst:
+        teleport_pairs[lst[0]] = lst[1]
+        teleport_pairs[lst[1]] = lst[0]
 
     if player_pos is None:
         print("Ошибка: На уровне отсутствует начальная позиция игрока")
@@ -164,9 +171,9 @@ def load_level():
             print(row)
         sys.exit()
 
-    return player_pos, boxes, goals
+    return player_pos, boxes, goals, teleport_pairs
 
-player_pos, boxes, goals = load_level()
+player_pos, boxes, goals, teleport_pairs = load_level()
 
 # Создание окна
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -190,6 +197,8 @@ def draw_level():
                 elif current_level[y][x] == 6:  # Дверь
                     if not is_door_open():
                         screen.blit(door_img, rect)
+                elif current_level[y][x] == 7:  # Телепорт
+                    screen.blit(teleport_img, rect)
 
     # Отрисовка ящиков
     for box in boxes:
@@ -216,14 +225,19 @@ def is_door_open():
 
 # Функция для отображения загрузочного экрана
 def show_loading_screen():
-    loading_image = pygame.image.load("loading.jpg")
+    loading_image = pygame.image.load("data/loading.jpg")
     loading_image = pygame.transform.scale(loading_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
     screen.blit(loading_image, (0, 0))
     pygame.display.flip()
     pygame.time.wait(2000)
 
-# Функция для отображения меню выбора уровня
+up_arrow_img = load_image("up_arrow.png")
+down_arrow_img = load_image("down_arrow.jpg")
+arrow_size = (30, 30)
+up_arrow_img = pygame.transform.scale(up_arrow_img, arrow_size)
+down_arrow_img = pygame.transform.scale(down_arrow_img, arrow_size)
+
 def show_level_menu():
     screen.fill(BLACK)
     font = pygame.font.Font(None, 40)
@@ -239,25 +253,22 @@ def show_level_menu():
 
         # Отрисовка стрелки вверх, если есть уровни выше
         if start_index > 0:
-            up_arrow = font.render("↑", True, WHITE)
-            up_arrow_rect = up_arrow.get_rect(center=(SCREEN_WIDTH // 2, 100))
-            screen.blit(up_arrow, up_arrow_rect)
+            up_arrow_rect = up_arrow_img.get_rect(center=(SCREEN_WIDTH // 2, 110))
+            screen.blit(up_arrow_img, up_arrow_rect)
         else:
             up_arrow_rect = None
 
-        # Отрисовка уровней
         level_buttons = []
         for i in range(start_index, min(start_index + visible_levels, len(levels))):
             level_text = font.render(f"Уровень {i + 1}", True, WHITE)
-            level_rect = level_text.get_rect(center=(SCREEN_WIDTH // 2, 140 + (i - start_index) * 50))
-            screen.blit(level_text, level_rect)
-            level_buttons.append((level_rect, i))
+            text_rect = level_text.get_rect(center=(SCREEN_WIDTH // 2, 140 + (i - start_index) * 50))
+            screen.blit(level_text, text_rect)
+            level_buttons.append((text_rect, i))
 
         # Отрисовка стрелки вниз, если есть уровни ниже
         if start_index + visible_levels < len(levels):
-            down_arrow = font.render("↓", True, WHITE)
-            down_arrow_rect = down_arrow.get_rect(center=(SCREEN_WIDTH // 2, 140 + visible_levels * 50))
-            screen.blit(down_arrow, down_arrow_rect)
+            down_arrow_rect = down_arrow_img.get_rect(center=(SCREEN_WIDTH // 2, 370))
+            screen.blit(down_arrow_img, down_arrow_rect)
         else:
             down_arrow_rect = None
 
@@ -274,33 +285,22 @@ def show_level_menu():
                 elif event.y < 0 and start_index + visible_levels < len(levels):
                     start_index += 1
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # ЛКМ
+                if event.button == 1:
                     # Проверка, нажата ли стрелка вверх
                     if up_arrow_rect and up_arrow_rect.collidepoint(event.pos):
                         start_index -= 1
-                    # Проверка, нажата ли стрелка вниз
                     elif down_arrow_rect and down_arrow_rect.collidepoint(event.pos):
                         start_index += 1
-                    # Проверка, выбран ли уровень
                     for button, index in level_buttons:
                         if button.collidepoint(event.pos):
                             return index
 
-#Загрузка и воспроизведение музыки для меню
-pygame.mixer.music.load("menu_music.mp3")
-pygame.mixer.music.play(-1)  # Бесконечное воспроизведение
-
-# Основной игровой цикл
 running = True
+load_music()
 show_loading_screen()
 current_level_index = show_level_menu()
 current_level = levels[current_level_index]
-player_pos, boxes, goals = load_level()
-
-# Смена музыки на игровую
-pygame.mixer.music.stop()
-pygame.mixer.music.load("game_music.mp3")
-pygame.mixer.music.play(-1)
+player_pos, boxes, goals, teleport_pairs = load_level()
 
 while running:
     for event in pygame.event.get():
@@ -323,20 +323,27 @@ while running:
             new_player_pos[1] += move_y
 
             # Проверка на столкновение со стеной
-            if 0 <= new_player_pos[0] < GRID_WIDTH and 0 <= new_player_pos[1] < GRID_HEIGHT and current_level[new_player_pos[1]][new_player_pos[0]] != 1:
+            # Проверка перемещения игрока
+            if 0 <= new_player_pos[0] < GRID_WIDTH and 0 <= new_player_pos[1] < GRID_HEIGHT and \
+                    current_level[new_player_pos[1]][new_player_pos[0]] != 1:
                 # Проверка на столкновение с ящиком
                 for i, box in enumerate(boxes):
                     if new_player_pos == box:
                         new_box_pos = [box[0] + move_x, box[1] + move_y]
                         # Проверка, можно ли сдвинуть ящик
-                        if 0 <= new_box_pos[0] < GRID_WIDTH and 0 <= new_box_pos[1] < GRID_HEIGHT and current_level[new_box_pos[1]][new_box_pos[0]] != 1 and new_box_pos not in boxes:
+                        if 0 <= new_box_pos[0] < GRID_WIDTH and 0 <= new_box_pos[1] < GRID_HEIGHT and \
+                                current_level[new_box_pos[1]][new_box_pos[0]] != 1 and new_box_pos not in boxes:
                             boxes[i] = new_box_pos
                             player_pos = new_player_pos
-                        break  # Прекратить проверку ящиков, если один был сдвинут
+                        break
                 else:
                     # Проверка на возможность прохода через дверь
                     if current_level[new_player_pos[1]][new_player_pos[0]] != 6 or is_door_open():
-                        player_pos = new_player_pos
+                        # Проверка на телепорт
+                        if (new_player_pos[0], new_player_pos[1]) in teleport_pairs:
+                            player_pos = list(teleport_pairs[(new_player_pos[0], new_player_pos[1])])
+                        else:
+                            player_pos = new_player_pos
 
     draw_level()
     pygame.display.flip()
@@ -345,13 +352,11 @@ while running:
         current_level_index += 1
         if current_level_index < len(levels):
             current_level = levels[current_level_index]
-            player_pos, boxes, goals = load_level()
+            player_pos, boxes, goals, teleport_pairs = load_level()
         else:
             print("Вы прошли все уровни!")
             running = False
 
     clock.tick(FPS)
 
-# Остановка музыки при выходе из игры
-pygame.mixer.music.stop()
 pygame.quit()
